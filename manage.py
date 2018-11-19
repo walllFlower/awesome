31d1dce12d6b71cd4d770ba7f51e1f72bf1294a7
zengym <zengymf@yonyou.com>
2018-11-19 16:27:50 +0800

    应用程序外部可见

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 from app import create_app, db
-from flask_script import Manager, Shell
+from flask_script import Manager, Shell, Server
 from flask_migrate import Migrate, MigrateCommand
 
 app = create_app('default')
@@ -14,6 +14,10 @@ def make_shell_context():
 
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
+manager.add_command('runserver', Server(
+    host = '0.0.0.0',
+    port = '80'
+))
 
 if __name__ == '__main__':
-    manager.run(host='0.0.0.0', port='80')
+    manager.run()
